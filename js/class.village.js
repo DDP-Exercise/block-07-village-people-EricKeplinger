416d19fed1e4c8a1b1c96c48d97e02127b58cd39
Ricki2001 <s2510892043@students.fh-hagenberg.at>
2026-06-01 19:52:01 +0200

    der Stand nach der Unterrichtseinheit

--- js/class.village.js
+++ js/class.village.js
@@ -1,11 +1,65 @@
 "use strict";
-
+import Citizen from "./class.citizen.js";
+import NobleCitizen from "./class.nobleCitizen.js";
 /**
  * Create a Village class. Each village should have
  * - a name
  * - an array of its buildings
  * - an array of its citizens
  *
+ *
+ *
  * You can see in main.js what methods a village should provide.
  * implement them.
  */
+
+export default class Village{
+    constructor(name){
+        this.name = name;
+        this.buildings = [];
+        this.citizens = [];
+    }
+
+    addBuilding(name, capacity, noble = false){
+        this.building.push(noble ? new NobleBuilding(name, capacity) : new Building(name, capacity));
+
+    }
+
+    addCitizen(name, noble = false){
+        this.citizens.push(noble ? new NobleCitizen(name) : new Citizen(name));
+        this.shelterCitizen(citizen);
+    }
+
+    shelterTheWorthy(){
+        for(const citizen of this.citizens){
+            if(citizen.home === null){
+                this.shelterCitizen(citizen);
+            }
+        }
+    }
+
+    shelterCitizen(citizen){
+        for(const building of this.buildings){
+            if(building.addResident(citizen)){
+                return;
+            }
+        }
+    }
+
+    printCitizenDirectory(){
+        for(const building of this.buildings){
+            building.listAllResidents();
+        }
+        this.listAllHomeless();
+    }
+
+    listAllHomeless(){
+        console.log("%c Homeless People of " + this.name + ": " , "background-color. #a00; color: white");
+        for(const citizen of this.citizens){
+            if(citizen.home === null){
+                console.log(citizen);
+            }
+        }
+    }
+
+}
